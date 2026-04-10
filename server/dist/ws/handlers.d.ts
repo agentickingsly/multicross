3f7ac360ba697d78876e1c356cd9fc28031c537a
Kingsly <agentickingsly@gmail.com>
2026-04-10 02:23:26 -0400

    chore: remove node_modules and dist from tracking, fix gitignore

--- server/dist/ws/handlers.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Server } from "socket.io";
-import type { ClientToServerEvents, ServerToClientEvents } from "@multicross/shared";
-type CrosswordServer = Server<ClientToServerEvents, ServerToClientEvents>;
-export declare function registerWsHandlers(io: CrosswordServer): void;
-export {};
